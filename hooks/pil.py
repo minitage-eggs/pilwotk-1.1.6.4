e650c9e006d17917e033a95ac16523b0b774671d
kiorky <kiorky@cryptelium.net>
2009-10-21 21:41:52 +0200

    2.6.4

--- hooks/pil.py
+++ hooks/pil.py
@@ -1,22 +1,26 @@
 #add minitage libraries to search pathes
 import os
+import re
+
+ref = re.M|re.I|re.U
 def pil(options,buildout):
     cwd = os.getcwd()
     os.chdir(options['compile-directory'])
-    os.system("""
-SED=$(which gsed)
-if [ ! -f "$SED" ];then
-       SED=sed
-fi
-$SED -re "s:^(FREETYPE_ROOT.*)$:FREETYPE_ROOT=libinclude('%(freetype)s'):g" -i setup.py
-$SED -re "s:^(TIFF_ROOT.*)$:TIFF_ROOT=libinclude('%(tiff)s'):g" -i setup.py
-$SED -re "s:^(JPEG_ROOT.*)$:JPEG_ROOT=libinclude('%(jpeg)s'):g" -i setup.py
-$SED -re "s:^(ZLIB_ROOT.*)$:ZLIB_ROOT=libinclude('%(zlib)s'):g" -i setup.py
-              """%{'freetype':buildout['freetype']['location'],
-                   'jpeg':buildout['libjpeg']['location'],
-                   'tiff':buildout['libtiff']['location'],
-                   'zlib':buildout['zlib']['location'],
-                  }
-                  )
+    locations = {'freetype':buildout['freetype']['location'],
+                 'jpeg':buildout['libjpeg']['location'],
+                 'tiff':buildout['libtiff']['location'],
+                 'zlib':buildout['zlib']['location'],
+                } 
+    st = open('setup.py').read()
+    pregex = [("^(FREETYPE_ROOT.*)$", "FREETYPE_ROOT=libinclude('%(freetype)s')"%locations),
+          ("^(TIFF_ROOT.*)$",  "TIFF_ROOT=libinclude('%(tiff)s')"%locations),
+          ("^(JPEG_ROOT.*)$", "JPEG_ROOT=libinclude('%(jpeg)s')"%locations),
+          ("^(ZLIB_ROOT.*)$",  "ZLIB_ROOT=libinclude('%(zlib)s')"%locations),
+    ] 
+
+    for p, rep in pregex:
+        r= re.compile(p, ref)
+        st = r.sub(rep, st)
+    open('setup.py', 'w').write(st)
     os.chdir(cwd)
 
